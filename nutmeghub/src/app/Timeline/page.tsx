2b526ce9d30790648f3fdee20f871b08526ab2d6
kamijo-haruto <kamijoharuto@gmail.com>
2024-09-20 15:43:43 +0900

    [add] ホーム、ポスト、タイムラインのページを追加

---
+++ nutmeghub/src/app/Timeline/page.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/Styles/timeline.module.css";
+
+export default function Page() {
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1>Timeline</h1>
+            </main>
+        </div>
+    );
+}
